26488847409125b23c38c4dd3e131eb5be2318ec
aleshaujay <aleshaujay@icloud.com>
2024-08-10 18:52:57 +0700

    Initial Commit

---
+++ TabBarApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabBarApp
+//
+//  Created by Lex Koby on 10.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
